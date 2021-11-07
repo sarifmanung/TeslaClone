debd30593fa020bd4dc27668a47511fa88a2cba7
sarif manung <406168026@yru.ac.th>
2021-11-07 16:25:15 +0700

    build and change logo

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,13 @@ function Home() {
         leftBtnText="Custom order"
         rightBtnText="Exisiting inventory"
       />
-      <Section
+        <Section
+        title="Model Y"
+        description="Order Online fro Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Exisiting inventory"
+      /><Section
         title="Model 3"
         description="Order Online fro Touchless Delivery"
         backgroundImg="model-3.jpg"
@@ -26,13 +32,7 @@ function Home() {
         leftBtnText="Custom order"
         rightBtnText="Exisiting inventory"
       />
-      <Section
-        title="Model Y"
-        description="Order Online fro Touchless Delivery"
-        backgroundImg="model-y.jpg"
-        leftBtnText="Custom order"
-        rightBtnText="Exisiting inventory"
-      />
+    
       <Section
         title="Lowerst Cost Solar Panels in America"
         description="Money-back guarantee"
