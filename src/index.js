4489af27d5968fb9eeed816dda6582e9e0602592
sarifmanung <406168026@yru.ac.th>
2023-12-20 16:39:14 +0700

    updated

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>{" "}
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
